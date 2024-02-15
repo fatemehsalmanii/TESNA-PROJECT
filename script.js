f5f33675a12fc3ec321c5a52105bb43d9207d131
fatemesalmani <fatemesalmaniii3876@gmail.com>
2024-02-15 12:29:01 +0330

    add edit

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let taskCounter = 0; // Add this line to initialize the counter
+let taskCounter = 1; // Add this line to initialize the counter
 
 let taskBar = document.querySelector(".active");
 let cardContainer = document.querySelector(".card--container");
@@ -21,16 +21,15 @@ let textarea = document.querySelector(".textarea");
 let inptCategory = document.querySelector(".category");
 let showCategory = document.querySelector(".show-category");
 let inptResponsible = document.querySelector(".responsible");
+let inputID = document.querySelector(".inp-id");
 let inptTitel = document.querySelector(".inp-titel");
-let editButton  = document.querySelector(".edit-btn");
+let editButton = document.querySelector(".edit-btn");
 let btnCancel = document.querySelector(".btn-cancel");
 
-
- 
-btnCancel.addEventListener('click', () => {
+btnCancel.addEventListener("click", () => {
   modal.style.display = "none";
   backDtap.style.display = "none";
-})
+});
 taskBar.addEventListener("click", () => {
   cardContainer.style.opacity = "1";
 });
@@ -38,47 +37,56 @@ taskBar.addEventListener("click", () => {
 addBtn.addEventListener("click", () => {
   modal.style.display = "block";
   backDtap.style.display = "block";
- 
+  inputID.value = "0";
+  inptTitel.value = "";
+  textarea.value = "";
 });
+
 taskrow.addEventListener("click", (e) => {
   modal.style.display = "block";
   backDtap.style.display = "block";
 
-  // let saveTasks = JSON.parse(localStorage.getItem("tasks")) || [];
-  // const taskID = e.target.parentElement.children[0].innerText;
-  // const taskTitle = e.target.parentElement.children[1].innerText;
-  // const taskDescription = e.target.parentElement.children[2].innerText;
-
-  const taskID = e.target.closest(".task-row").id.split("-")[1];
-  const taskTitle = e.target.closest(".task-row").querySelectorAll(".task-title")[1].innerText;
-  const taskDescription = e.target.closest(".task-row").querySelectorAll(".task-title")[2].innerText;
-  
+  const taskID = e.target
+    .closest(".task-row")
+    .querySelectorAll(".task-title")[0].innerHTML;
+  const taskTitle = e.target
+    .closest(".task-row")
+    .querySelectorAll(".task-title")[1].textContent;
+  const taskDescription = e.target
+    .closest(".task-row")
+    .querySelectorAll(".task-title")[2].textContent;
 
   // Set the input fields to the current task's values
+  inputID.value = taskID;
   inptTitel.value = taskTitle;
   textarea.value = taskDescription;
 
-
   // Edit the task when the user clicks the save button
-  const saveBtn = document.querySelector(".btn-save");
+  const saveBtn = document.querySelector(".btn-add");
   saveBtn.addEventListener("click", () => {
     const newTitle = inptTitel.value;
     const newDescription = textarea.value;
+    const taskID = e.target
+      .closest(".task-row")
+      .querySelectorAll(".task-title")[0].innerText;
     editLocalTasks(taskID, newTitle, newDescription);
     modal.style.display = "none";
     backDtap.style.display = "none";
   });
 });
+
 function editLocalTasks(id, newTitle, newDescription) {
   let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
   for (let i = 0; i < tasks.length; i++) {
-    if (tasks[i].id === id) {
+    if (tasks[i].id == id) {
       tasks[i].title = newTitle;
       tasks[i].description = newDescription;
       break;
     }
   }
   localStorage.setItem("tasks", JSON.stringify(tasks));
+
+  // getLocalTasks();
 }
 
 backDtap.addEventListener("click", () => {
@@ -101,8 +109,7 @@ assigne.addEventListener("click", () => {
 reminder.addEventListener("click", () => {
   thirdPopup.style.display = "flex";
 });
-document.addEventListener("DOMContentLoaded", getLocalTasks);
-
+ document.addEventListener("DOMContentLoaded", getLocalTasks);
 
 // date
 dateInput.addEventListener("change", (event) => {
@@ -126,15 +133,16 @@ inptResponsible.addEventListener("change", (e) => {
 
 btnAdd.addEventListener("click", addTask);
 
-
 function addTask(e) {
   e.preventDefault();
 
-  let taskDiv = document.createElement("div");
-  taskDiv.classList.add("task-row");
-  taskDiv.id = "task-" + taskCounter; // Add this line to assign a unique ID to each task
+  const taskID = inputID.value;
 
-  const newTodo = ` 
+  if (taskID == 0) {
+    let taskDiv = document.createElement("div");
+    taskDiv.classList.add("task-row");
+    taskDiv.id = "task-" + taskCounter; // Add this line to assign a unique ID to each task
+    const newTodo = ` 
 <i class='bx bx-checkbox'></i>
 <li class="task-title">${taskCounter}</li>
 <li class="task-title">${inptTitel.value}</li>
@@ -142,27 +150,32 @@ function addTask(e) {
 <li class="task-title">${inptCategory.value}</li>
 <li class="task-title">${inptResponsible.value}</li>
 <li class="task-title">${dateInput.value}</li>`;
-  taskDiv.innerHTML = newTodo;
-  taskrow.appendChild(taskDiv);
-  // saveLocalTodos(todoInput.value)
-  const newTask = {
-    id: taskCounter, // Add this line to include the unique ID in the tasks object
-    title: inptTitel.value,
-    textarea: textarea.value,
-    catagory: inptCategory.value,
-    responsible: inptResponsible.value,
-    date: dateInput.value,
-  };
-  saveLocalTasks(newTask);
-  inptTitel.value = "";
-  textarea.value = "";
-  inptCategory.value = "";
-  inptResponsible.value = "";
-  dateInput.value = "";
-  modal.style.display = "none";
-  backDtap.style.display = "none";
+    taskDiv.innerHTML = newTodo;
+    taskrow.appendChild(taskDiv);
+    // saveLocalTodos(todoInput.value)
+    const newTask = {
+      id: taskCounter, // Add this line to include the unique ID in the tasks object
+      title: inptTitel.value,
+      textarea: textarea.value,
+      catagory: inptCategory.value,
+      responsible: inptResponsible.value,
+      date: dateInput.value,
+    };
+    saveLocalTasks(newTask);
+    inptTitel.value = "";
+    textarea.value = "";
+    inptCategory.value = "";
+    inptResponsible.value = "";
+    dateInput.value = "";
+    modal.style.display = "none";
+    backDtap.style.display = "none";
 
-  taskCounter++; // Increment the counter after adding the task
+    taskCounter++; // Increment the counter after adding the task
+  } else {
+    const newTitle = inptTitel.value;
+    const newDescription = textarea.value;
+    editLocalTasks(taskID, newTitle, newDescription);
+  }
 }
 
 //Remove  Tasks
@@ -171,11 +184,14 @@ function removeChek(e) {
   const item = e.target;
 
   if (classList[1] === "bx-checkbox") {
-    const taskTitle = e.target.closest(".task-row").querySelectorAll(".task-title")[0];
-    const taskDescription = e.target.closest(".task-row").querySelectorAll(".task-title")[2];
+    const taskTitle = e.target
+      .closest(".task-row")
+      .querySelectorAll(".task-title")[0];
+    const taskDescription = e.target
+      .closest(".task-row")
+      .querySelectorAll(".task-title")[2];
 
     if (taskTitle && taskDescription) {
-      debugger;
       const taskID = taskTitle.innerText;
       removeLocalTasks(taskID);
 
@@ -186,18 +202,16 @@ function removeChek(e) {
   }
 }
 
-
 //LOCAL
 function saveLocalTasks(task) {
   let saveTasks = localStorage.getItem("tasks")
     ? JSON.parse(localStorage.getItem("tasks"))
     : [];
-    //********/
   saveTasks.push(task);
   localStorage.setItem("tasks", JSON.stringify(saveTasks));
 }
-//show in DOM
 
+//show in DOM
 function getLocalTasks() {
   let saveTasks = JSON.parse(localStorage.getItem("tasks")) || [];
   saveTasks.forEach((task) => {
@@ -211,7 +225,7 @@ function getLocalTasks() {
       <li class="task-title">${task.catagory}</li>
       <li class="task-title">${task.responsible}</li>
       <li class="task-title">${task.date}</li>`;
-    taskCounter=task.id+1;
+    taskCounter = task.id + 1;
     taskDiv.innerHTML = newTodo;
     taskrow.appendChild(taskDiv);
   });
@@ -219,16 +233,12 @@ function getLocalTasks() {
 
 //delete from Local Storage
 function removeLocalTasks(id) {
-  debugger
   let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
   for (let i = 0; i < tasks.length; i++) {
     if (tasks[i].id == id) {
-      debugger
       tasks.splice(i, 1);
       localStorage.setItem("tasks", JSON.stringify(tasks));
       break;
     }
   }
 }
-
-
